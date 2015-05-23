2bd83668618fc52a837d43205b35db88f0085461
Fayland Lam <fayland@gmail.com>
2015-05-23 10:34:33 +0800

    upper case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -21,21 +21,21 @@ class TestCase extends PHPUnit_Framework_TestCase
     // test delimiter
     public function testGeneral()
     {
-        $this->assertEquals('nín hǎo', Pinyin::pinyin('您好'));
-        $this->assertEquals('dài zhe xī wàng qù lǚ xíng ， bǐ dào dá zhōng diǎn gèng měi hǎo', Pinyin::pinyin('带着希望去旅行，比到达终点更美好'));
-        $this->assertEquals('cè shì R60', Pinyin::pinyin("测试R60"));
+        $this->assertEquals('nín hǎo', PinYin::pinyin('您好'));
+        $this->assertEquals('dài zhe xī wàng qù lǚ xíng ， bǐ dào dá zhōng diǎn gèng měi hǎo', PinYin::pinyin('带着希望去旅行，比到达终点更美好'));
+        $this->assertEquals('cè shì R60', PinYin::pinyin("测试R60"));
     }
 
     public function testTemporaryDelimiter() {
-        $this->assertEquals('nín-hǎo', Pinyin::pinyin('您好', array('delimiter' => '-')));
-        Pinyin::set('delimiter', '*');
-        $this->assertEquals('nín*hǎo', Pinyin::pinyin('您好'));
-        Pinyin::set('delimiter', ' ');
-        $this->assertEquals('nín hǎo', Pinyin::pinyin('您好'));
+        $this->assertEquals('nín-hǎo', PinYin::pinyin('您好', array('delimiter' => '-')));
+        PinYin::set('delimiter', '*');
+        $this->assertEquals('nín*hǎo', PinYin::pinyin('您好'));
+        PinYin::set('delimiter', ' ');
+        $this->assertEquals('nín hǎo', PinYin::pinyin('您好'));
     }
 
     public function testResultWithoutTone()
     {
-        $this->assertEquals('dai zhe xi wang qu lü xing ， bi dao da zhong dian geng mei hao', Pinyin::pinyin('带着希望去旅行，比到达终点更美好', array('tone' => false)));
+        $this->assertEquals('dai zhe xi wang qu lü xing ， bi dao da zhong dian geng mei hao', PinYin::pinyin('带着希望去旅行，比到达终点更美好', array('tone' => false)));
     }
 }
